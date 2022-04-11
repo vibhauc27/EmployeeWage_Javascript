d21b20e564f17bebcef8ab3deee3fff44a685c7d
Vibhauc_27 <ucvibha@gmail.com>
2022-04-11 21:07:50 +0530

    [Vibha]Added:To calculate emp wage till maximum working hours and days meet

--- EmpWage.js
+++ EmpWage.js
@@ -1,6 +1,6 @@
 console.log("Welcome to Employee Wage Program using java script")
 
-// Ability to Calculate Daily Employee Wage based on part time or full time work.
+// Calculate Daily Employee Wage based on part time or full time work.
 const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -8,14 +8,17 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NO_OF_WORKING_DAYS = 20;
+const MAX_HOURS_IN_MONTH = 160;
 {
     let totalEmpHrs = 0;
-    for(let day=0; day<NO_OF_WORKING_DAYS; day++)
+    let totalWorkingDays = 0;
+    while( totalWorkingDays<NO_OF_WORKING_DAYS && totalEmpHrs<=MAX_HOURS_IN_MONTH ) 
     {
         let empCheck = Math.floor(Math.random() * 10) % 3;
         let empHrs=getWorkHours(empCheck);
         totalEmpHrs += empHrs;
-    }
+        totalWorkingDays++
+    } 
 // Check Employee is present or Absent or part time       
     function getWorkHours(empCheck)
     {
@@ -36,5 +39,5 @@ const NO_OF_WORKING_DAYS = 20;
         }
     }
 let empWage = totalEmpHrs * WAGE_PER_HOUR; 
-console.log("Emp Work Hrs: "+ totalEmpHrs +", Emp Wage: " + empWage); 
+console.log("Total Days:" + totalWorkingDays + " Total Hrs:" + totalEmpHrs + " Emp Wage:" + empWage); 
 }
