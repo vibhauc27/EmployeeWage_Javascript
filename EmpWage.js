f821526b90c2f78e11b8a19af97415e7d87e46b0
Vibhauc_27 <ucvibha@gmail.com>
2022-04-11 17:15:22 +0530

    [Vibha]Added:To calculate the monthly employee wage

--- EmpWage.js
+++ EmpWage.js
@@ -1,18 +1,22 @@
 console.log("Welcome to Employee Wage Program using java script")
 
-// UC3 - function to get work hours.
+// Ability to Calculate Daily Employee Wage based on part time or full time work.
 const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-
+const NO_OF_WORKING_DAYS = 20;
 {
-    let empHrs = 0;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs=getWorkHours(empCheck);
-
+    let totalEmpHrs = 0;
+    for(let day=0; day<NO_OF_WORKING_DAYS; day++)
+    {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        let empHrs=getWorkHours(empCheck);
+        totalEmpHrs += empHrs;
+    }
+// Check Employee is present or Absent or part time       
     function getWorkHours(empCheck)
     {
         switch (empCheck) 
@@ -31,6 +35,6 @@ const WAGE_PER_HOUR = 20;
                 break;
         }
     }
-let empWage = empHrs * WAGE_PER_HOUR; 
-console.log("Emp Work Hrs: "+ empHrs +", Emp Wage: " + empWage); 
+let empWage = totalEmpHrs * WAGE_PER_HOUR; 
+console.log("Emp Work Hrs: "+ totalEmpHrs +", Emp Wage: " + empWage); 
 }
