9c7dd4f0e3801a10ca545ee48e1d16aabba62fbb
Vibhauc_27 <ucvibha@gmail.com>
2022-04-11 17:09:05 +0530

    [Vibha]Added:To get the working hours

--- EmpWage.js
+++ EmpWage.js
@@ -1,21 +1,6 @@
 console.log("Welcome to Employee Wage Program using java script")
 
-//UC1 - Check Employee is present or Absent using Math.Random
-{
-    const IS_ABSENT=0;
-    let empcheck = Math.floor(Math.random()*10)%2;
-    if(empcheck==IS_ABSENT)
-    {
-        console.log("Employee is Absent");
-    }
-    else
-    {
-        console.log("Employee is Present");
-    }
-}
-
-
-// UC2 - Ability to Calculate Daily Employee Wage based on part time or full time work.
+// UC3 - function to get work hours.
 const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -26,21 +11,26 @@ const WAGE_PER_HOUR = 20;
 {
     let empHrs = 0;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    switch (empCheck) 
+    empHrs=getWorkHours(empCheck);
+
+    function getWorkHours(empCheck)
     {
-        case IS_ABSENT:
-            empHrs = 0;
-            break;            
-        case IS_PART_TIME:
-            empHrs = PART_TIME_HOURS;
-            break;
-        case IS_FULL_TIME:
-            empHrs = FULL_TIME_HOURS; 
-            break;
-        default:
-            empHrs = 0;
-            break;
+        switch (empCheck) 
+        {
+            case IS_ABSENT:
+                return 0;
+                break;
+            case IS_PART_TIME:
+                return PART_TIME_HOURS; 
+                break;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS; 
+                break;
+            default:
+                return 0;
+                break;
+        }
     }
-    let empWage = empHrs * WAGE_PER_HOUR; 
-    console.log("Emp Wage: " + empWage);
+let empWage = empHrs * WAGE_PER_HOUR; 
+console.log("Emp Work Hrs: "+ empHrs +", Emp Wage: " + empWage); 
 }
